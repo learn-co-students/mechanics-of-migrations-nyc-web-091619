0ab6bd656f01464b7b3bde75f2151456651b00b9
Anya <anya.s.litvinova@gmail.com>
2019-09-27 15:11:26 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
-
+#DB = {:conn => SQLite3::Database.new("db/migrate/01_create_artists.rb")}
 # put the code to connect to the database here
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
